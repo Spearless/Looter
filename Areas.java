9dcf4af45c3732217e7cb75fd93e46139f10bf9a
Spearless <gaston.ezequiel.ortega@gmail.com>
2019-10-17 01:28:50 -0300

    Updated getters.

--- Areas.java
+++ Areas.java
@@ -1,11 +1,28 @@
-package bot;
+package bot.Looter;
 
 import com.runemate.game.api.hybrid.location.Area;
 import com.runemate.game.api.hybrid.location.Coordinate;
 
 public class LooterAreas {
-    Area area= new Area.Rectangular(new Coordinate(3083,3538,0),new Coordinate(3096,3530,0));
-    Area safeArea= new Area.Rectangular(new Coordinate(3087,3525,0),new Coordinate(3097,3523,0));
-    Area bankArea= new Area.Rectangular(new Coordinate(3091,3498),new Coordinate(3099,3487));
-    Area BesideTheDitch= new Area.Rectangular(new Coordinate(3078,3521), new Coordinate(3115,3515));
+    private Area area= new Area.Rectangular(new Coordinate(3083,3538,0),new Coordinate(3096,3530,0));
+    private Area safeArea= new Area.Rectangular(new Coordinate(3087,3525,0),new Coordinate(3097,3523,0));
+    private Area bankArea= new Area.Rectangular(new Coordinate(3091,3498),new Coordinate(3099,3487));
+    private Area BesideTheDitch= new Area.Rectangular(new Coordinate(3078,3521), new Coordinate(3115,3515));
+
+
+    public Area getArea() {
+        return area;
+    }
+
+    public Area getSafeArea() {
+        return safeArea;
+    }
+
+    public Area getBankArea() {
+        return bankArea;
+    }
+
+    public Area getBesideTheDitch() {
+        return BesideTheDitch;
+    }
 }
